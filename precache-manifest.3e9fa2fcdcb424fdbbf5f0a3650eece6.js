42f7ef37a3b3bbe700fa7ac314bd02cfb75c018d
yassinerzk <yassinerazzouki@gmail.com>
2020-02-22 20:13:51 +0100

    Updates

---
+++ precache-manifest.3e9fa2fcdcb424fdbbf5f0a3650eece6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fc1ba32bfaf50b4822ffa74cd5d7ab",
+    "url": "/ModernToDoList/index.html"
+  },
+  {
+    "revision": "6836673c9d3bb044774f",
+    "url": "/ModernToDoList/static/css/main.a6e90a0d.chunk.css"
+  },
+  {
+    "revision": "a10aeae300c9476136d7",
+    "url": "/ModernToDoList/static/js/2.50b01cab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ModernToDoList/static/js/2.50b01cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6836673c9d3bb044774f",
+    "url": "/ModernToDoList/static/js/main.09d4e9d6.chunk.js"
+  },
+  {
+    "revision": "1743ba5ca84039c3249a",
+    "url": "/ModernToDoList/static/js/runtime-main.2471112b.js"
+  }
+]);
